c2d91d03f6f07789cf9cde76fba2dd92237b1d3b
Claudio Ponziani <c.ponziani@isa.it>
2016-05-30 15:16:18 +0200

    add nabla folder

---
+++ customers/nabla/custom_nabla/__openerp__.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2014 ISA srl (<http://www.isa.it>)
+#    All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'NABLA - Customization module',
+    'version': '0.1',
+    'category': '',
+    'description': """
+    Il modulo contiente tutti i moduli che sono necessari per il cliente NABLA
+Personalizzazioni per Cliente Nabla
+=========================================
+Il modulo ha come dipendenze tutti i moduli di personalizzazione
+per il Cliente ASL-VT.
+
+       """,
+    'author': 'ISA srl',
+    'depends': [
+        'account',
+        'account_accountant',
+        'account_account_partner',
+        'account_analytic_analysis',
+        'account_chart',
+        'account_chart_b2pharma',
+        'account_followup',
+        'account_payment',
+        'account_voucher',
+        'analytic',
+        'analytic_user_function',
+        'auth_crypt',
+        'auth_signup',
+        'base',
+        'base_action_rule',
+        'base_iban',
+        'base_import',
+        'base_isa',
+        'base_location',
+        'base_location_geonames_import',
+        'base_setup',
+        'base_vat',
+        'board',
+        'bus',
+        'calendar',
+        'contacts',
+        'crm',
+        'crm_mass_mailing',
+        'decimal_precision',
+        'document',
+        'edi',
+        'email_template',
+        'fetchmail',
+        'fleet',
+        'google_account',
+        'google_calendar',
+        'hr',
+        'hr_attendance',
+        'hr_holidays',
+        'hr_timesheet',
+        'hr_timesheet_invoice',
+        'hr_timesheet_sheet',
+        'im_chat',
+        'knowledge',
+        'l10n_it',
+        'l10n_it_base',
+        'l10n_it_base_location_geonames_import',
+        'mail',
+        'marketing',
+        'marketing_campaign',
+        'marketing_crm',
+        'mass_mailing',
+        'mrp',
+        'note',
+        'payment',
+        'payment_transfer',
+        'pentaho_reports',
+        'portal',
+        'portal_project',
+        'portal_sale',
+        'portal_stock',
+        'procurement',
+        'procurement_manager',
+        'procurement_plan',
+        'procurement_plan_mrp',
+        'product',
+        'product_expiry',
+        'product_family',
+        'project',
+        'project_timesheet',
+        'purchase',
+        'report',
+        'resource',
+        'sale',
+        'sale_crm',
+        'sale_journal',
+        'sale_margin',
+        'sale_mrp',
+        'sale_stock',
+        'sales_team',
+        'share',
+        'stock',
+        'stock_account',
+        'warning',
+        'web',
+        'web_calendar',
+        'web_diagram',
+        'web_gantt',
+        'web_graph',
+        'web_kanban',
+        'web_kanban_gauge',
+        'web_kanban_sparkline',
+        'web_tests',
+        'web_view_editor',
+        'website',
+        'website_mail',
+        'website_report',
+        'website_sale',
+    ],
+    'data': [],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'active': False,
+    'certificate': '',
+}
