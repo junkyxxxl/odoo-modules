e42bb3def460054721327bcd930e743b89094245
Claudio Ponziani <c.ponziani@isa.it>
2016-05-30 16:53:11 +0200

    move pob modules

--- third-party/prestashop-connector/pob_extension_stock/prestapi/__init__.py
+++
@@ -1,4 +0,0 @@
-from prestapi import PrestaShopWebService
-from prestapi import PrestaShopWebServiceDict
-from prestapi import PrestaShopWebServiceError
-from prestapi import PrestaShopAuthenticationError
