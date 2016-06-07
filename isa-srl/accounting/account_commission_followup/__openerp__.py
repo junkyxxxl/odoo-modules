0f7b3d35c74701df7a84f189c333e635dbf5911b
DanieleMinnocci1989 <d.minnocci@isa.it>
2016-06-07 09:09:37 +0200

    [account_invoice_kit], [account_commission], [account_commission_followup]
    Update about account_invoice_kit for set a total_invoice;
    Create and update account_commission for the salesagent's permissions.

---
+++ isa-srl/accounting/account_commission_followup/__openerp__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "account_commission_followup",
+
+    'summary': """
+        Questo modulo integra le funzioni del modulo account_commission e account_follow_up""",
+
+    'description': """
+        Questo modulo integra le funzioni del modulo account_commission e account_follow_up
+    """,
+
+    'author': "Isa-Srl",
+    'website': "http://www.isa.it",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Accounting & Finance',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base',
+                'account',
+                'account_commission',
+                'account_followup'],
+
+    # always loaded
+    'data': ['views/res.partner.xml',
+             'security/commission_security.xml',
+             'security/ir.model.access.csv',
+             'security/ir.rule.csv'
+             ],
+    'auto_install': True,
+}
