d25d36ecaaf5c7699e1007ef14881b0df5a30e8b
valentina <v.giuliani@isa.it>
2016-06-10 10:09:26 +0200

    Modifiche hr_report

--- isa-srl/hr/hr_holidays_days_contract/flag_working_hour.py
+++ isa-srl/hr/hr_holidays_days_contract/flag_working_hour.py
@@ -5,9 +5,4 @@ import datetime
 class flag(models.TransientModel):
     _inherit = 'hr.config.settings'
 
-    @api.model
-    def get_manage_working_hour(self):
-        stock_config_obj = self.env['hr.config.settings'].search([], limit=1, order="id DESC")
-        return stock_config_obj.manage_working_hour
-
     module_hr_holidays_working_hour = fields.Boolean('Gestisci i permessi in ore', help="Installa il modulo hr_holidays_working_hour")
